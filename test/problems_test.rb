4c4c43435a0973cf5dc4912dc6f07751fcd9383f
Ernesto <e.ernesto.espinosa@gmail.com>
2021-11-02 04:10:48 -0600

    Add test for executable gem

--- test/problems_test.rb
+++ test/problems_test.rb
@@ -6,4 +6,8 @@ class ProblemsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Problems::VERSION
   end
+
+  def test_gem_is_executable
+    refute_includes `problems`, 'command not found'
+  end
 end
